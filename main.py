19f7702107e23c2d8342030e773ba263b2914144
GenericName192 <David-barton192@hotmail.co.uk>
2026-01-05 15:15:32 +0000

    Second lesson - return number of words in frankenstein

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def get_book_text(path_to_file):
         return f.read()
 
 
+def number_of_words_in_book(book):
+    return len(book.split())
+
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    # print(get_book_text("books/frankenstein.txt"))
+    book = get_book_text("books/frankenstein.txt")
+    num_words = number_of_words_in_book(book)
+    print(f"Found {num_words} total words")
 
 
 main()
