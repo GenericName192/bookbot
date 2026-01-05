11af4b148d1b6b5c5588f38ec952cfc9878e7313
GenericName192 <David-barton192@hotmail.co.uk>
2026-01-05 16:32:13 +0000

    lesson 4 - create a dict with each char and how often they turn up

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import number_of_words_in_book
+from stats import number_of_words_in_book, number_of_each_character
 
 
 def get_book_text(path_to_file):
@@ -11,6 +11,7 @@ def main():
     book = get_book_text("books/frankenstein.txt")
     num_words = number_of_words_in_book(book)
     print(f"Found {num_words} total words")
+    print(number_of_each_character(book))
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,14 @@
 def number_of_words_in_book(book):
     return len(book.split())
+
+
+def number_of_each_character(book):
+    book = book.lower()
+    char_count = {}
+    for character in book:
+        if character in char_count:
+            char_count[character] += 1
+        else:
+            char_count[character] = 1
+
+    return char_count
