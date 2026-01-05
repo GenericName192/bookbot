68ca3be0a4a91e77cd173f0f53a90117b5212e7d
GenericName192 <David-barton192@hotmail.co.uk>
2026-01-05 17:30:11 +0000

    lesson 6 remove hardcoding and make the program run off system arguements instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import number_of_words_in_book, number_of_each_character
 from stats import sorted_character_numbers
+import sys
 
 
 def get_book_text(path_to_file):
@@ -8,20 +9,23 @@ def get_book_text(path_to_file):
 
 
 def main():
-    # print(get_book_text("books/frankenstein.txt"))
-    book = get_book_text("books/frankenstein.txt")
-    num_words = number_of_words_in_book(book)
-    number_of_chars = number_of_each_character(book)
-    sorted_list = sorted_character_numbers(number_of_chars)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for dict in sorted_list:
-        if dict["char"].isalpha():
-            print(f"{dict["char"]}: {dict["num"]}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book = get_book_text(sys.argv[1])
+        num_words = number_of_words_in_book(book)
+        number_of_chars = number_of_each_character(book)
+        sorted_list = sorted_character_numbers(number_of_chars)
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for dict in sorted_list:
+            if dict["char"].isalpha():
+                print(f"{dict["char"]}: {dict["num"]}")
+        print("============= END ===============")
 
 
 main()
